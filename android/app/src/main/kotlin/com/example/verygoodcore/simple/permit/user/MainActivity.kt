d9cd1ef7d3eebc2008309819ec5af402a9630edb
Muhammad Abdi Alidrus <muhammadabdi25@gmail.com>
2024-11-05 10:13:31 +0700

    initial commit

---
+++ android/app/src/main/kotlin/com/example/verygoodcore/simple/permit/user/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.verygoodcore.simple_permit_user
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main_development.dart
@@ -0,0 +1,6 @@
+import 'package:simple_permit_user/app/app.dart';
+import 'package:simple_permit_user/bootstrap.dart';
+
+void main() {
+  bootstrap(() => const App());
+}
