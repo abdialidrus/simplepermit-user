f8927d66da7f37e5b03343622b1b6e6bbbfb9125
Muhammad Abdi Alidrus <muhammadabdi25@gmail.com>
2024-11-06 16:24:55 +0700

    update permit repository package

--- packages/permit_repository/lib/permit_repository.dart
+++ packages/permit_repository/lib/permit_repository.dart
@@ -1,4 +1,5 @@
 /// A repository that handles permit related requests.
 library;
 
+export 'package:permit_api/permit_api.dart' show Permit;
 export 'src/permit_repository.dart';
--- packages/permit_repository/lib/src/permit_repository.dart
+++ packages/permit_repository/lib/src/permit_repository.dart
@@ -1,7 +1,28 @@
+import 'package:permit_api/permit_api.dart';
+
 /// {@template permit_repository}
 /// A repository that handles permit related requests.
 /// {@endtemplate}
 class PermitRepository {
   /// {@macro permit_repository}
-  const PermitRepository();
+  const PermitRepository({
+    required PermitApi permitApi,
+  }) : _permitApi = permitApi;
+
+  final PermitApi _permitApi;
+
+  /// Provides a [Stream] of all permits.
+  Stream<List<Permit>> getPermits() => _permitApi.getPermits();
+
+  /// Saves a [permit].
+  ///
+  /// If a [permit] with the same [Permit.id] already exists, it will be
+  /// overwritten.
+  Future<void> savePermit(Permit permit) => _permitApi.savePermit(permit);
+
+  /// Deletes a `permit` with the given [id].
+  ///
+  /// If no `permit` with the given id exists, a [PermitNotFoundException] will
+  /// be thrown.
+  Future<void> deletePermit(String id) => _permitApi.deletePermit(id);
 }
